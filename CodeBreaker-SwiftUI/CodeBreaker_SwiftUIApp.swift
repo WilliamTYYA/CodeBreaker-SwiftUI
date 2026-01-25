e05f7da6506cf8c47a0b22b44321240d42e71f5f
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2026-01-24 19:34:05 -0600

    Initial Commit

---
+++ CodeBreaker-SwiftUI/CodeBreaker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_SwiftUIApp.swift
+//  CodeBreaker-SwiftUI
+//
+//  Created by Thiha Ye Yint Aung on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
