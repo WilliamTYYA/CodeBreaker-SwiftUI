51ac3cb37ee1fa3f4312732ec0901c96ac55b49f
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2026-01-26 02:33:38 -0600

    add views and ui extensions

---
+++ CodeBreaker-SwiftUI/View/PegChooser.swift
@@ -0,0 +1,37 @@
+//
+//  PegChooser.swift
+//  CodeBreaker-SwiftUI
+//
+//  Created by Thiha Ye Yint Aung on 1/26/26.
+//
+
+import SwiftUI
+
+struct PegChooser: View {
+    // MARK: Data In
+    let choices: [Peg]
+    
+    // MARK: Data Out Function
+    let onChoose: ((Peg) -> Void)?
+    
+    // MARK: - Body
+    
+    var body: some View {
+        HStack {
+            ForEach(choices, id: \.self) { peg in
+                Button {
+                    onChoose?(peg)
+                } label: {
+                    PegView(peg: peg)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PegChooser(choices: [Color.red, .blue, .green, .yellow]) { peg in
+        print("chose \(peg)")
+    }
+    .padding()
+}
---
+++ CodeBreaker-SwiftUI/View/PegView.swift
@@ -0,0 +1,29 @@
+//
+//  PegView.swift
+//  CodeBreaker-SwiftUI
+//
+//  Created by Thiha Ye Yint Aung on 1/26/26.
+//
+
+import SwiftUI
+
+struct PegView: View {
+    // MARK: Data In
+    let peg: Peg
+    
+    // MARK: - Body
+    
+    let pegShape = Circle()
+
+    var body: some View {
+        pegShape
+            .contentShape(pegShape)
+            .aspectRatio(1, contentMode: .fit)
+            .foregroundStyle(peg)
+    }
+}
+
+#Preview {
+    PegView(peg: Color.blue)
+        .padding()
+}
